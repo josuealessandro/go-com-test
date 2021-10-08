b8f6d8bf8d9174a22b3b2fffc6d41d28c56225f3
Josué Alessandro <josue.omena@enjoei.com.br>
2021-10-08 14:07:00 -0300

    Criando metodo auxiliar para os testes

--- src/github.com/josuealessandro/ola/ola_test.go
+++ src/github.com/josuealessandro/ola/ola_test.go
@@ -3,22 +3,26 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
+	verificaMensagemCorreta := func(t *testing.T, resultado string, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
+
 	t.Run("Quando o nome é informado", func(t *testing.T) {
 		resultado := Ola("Josué")
 		esperado := "Olá, Josué"
 
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("Quando o nome não é informado", func(t *testing.T) {
 		resultado := Ola("")
 		esperado := "Olá, Mundo"
 
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
+
 	})
 
 }
