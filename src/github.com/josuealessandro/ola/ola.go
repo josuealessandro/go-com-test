8b4182b93831057febfb6afd89ca7b8c7e95bb22
Josué Alessandro <josue.omena@enjoei.com.br>
2021-10-08 14:38:03 -0300

    deixando a função privada

--- src/github.com/josuealessandro/ola/ola.go
+++ src/github.com/josuealessandro/ola/ola.go
@@ -10,10 +10,10 @@ func Ola(nome string, idioma string) string {
 	if nome == "" {
 		nome = "Mundo"
 	}
-	return PrefixodeSaudacao(idioma) + nome
+	return prefixodeSaudacao(idioma) + nome
 }
 
-func PrefixodeSaudacao(idioma string) (prefixo string) {
+func prefixodeSaudacao(idioma string) (prefixo string) {
 	switch idioma {
 	case "frances":
 		prefixo = prefixoOlaFrances
